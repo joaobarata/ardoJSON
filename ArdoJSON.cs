5ce89ac5633cb5272d7df8284399d132e4f7a5c3
joaobarata <14177189+joaobarata@users.noreply.github.com>
2024-01-31 14:51:09 +0000

    Initial Commit

---
+++ ArdoJSON.cs
@@ -0,0 +1,184 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace Ardo.ArdoJSON
+{
+    public class ArdoJSON : IArdoJSON
+    {
+
+        public string JSON_Listify(string JSONIn, string? Path = "")
+        {
+
+            string[] path = Array.Empty<string>();
+            if (Path != null && Path.Length > 0)
+            {
+                path = Path.Trim().Split('.');
+            }
+            JToken root = Inner_Listify(JToken.Parse(JSONIn), path, 0);
+
+            StringBuilder sb = new StringBuilder();
+
+            using (JsonWriter json = new JsonTextWriter(new StringWriter(sb)))
+            {
+                json.Formatting = Formatting.None;
+                json.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                root.WriteTo(json);
+            }
+
+            return sb.ToString();
+        }
+
+        public string JSON_Objectify(string JSONIn, string? Path = "")
+        {
+            string[] path = Array.Empty<string>();
+            if (Path != null && Path.Length > 0)
+            {
+                path = Path.Trim().Split('.');
+            }
+            JToken root = Inner_Objectify(JToken.Parse(JSONIn), path, 0);
+
+            StringBuilder sb = new StringBuilder();
+
+            using (JsonWriter json = new JsonTextWriter(new StringWriter(sb)))
+            {
+                json.Formatting = Formatting.None;
+                json.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                root.WriteTo(json);
+            }
+
+            return sb.ToString(); ;
+        }
+        public string JSONSelect(string JSONIn, string? Path = "", bool? AlwaysReturnAsJSON = false)
+        {
+            var o = JToken.Parse(JSONIn);
+            var selEnum = o.SelectTokens(Path?? "");
+
+            bool returnJson = AlwaysReturnAsJSON ?? false;
+
+            string result;
+            if (selEnum.Count() == 1)
+            {
+
+                result = JsonConvert.SerializeObject(selEnum.ElementAt(0));
+                if (!returnJson && selEnum.First().Children().Count() == 0)
+                {
+                    result = result.Trim('"');
+                }
+            }
+            else
+            {
+                result = JsonConvert.SerializeObject(selEnum);
+            }
+
+            return result;
+        }
+
+        private JToken Inner_Objectify(JToken root, string[] path, int index)
+        {
+            if (path.Length == index)
+            {
+                // nothing to do if we're not at an array
+                if (root.Type != JTokenType.Array)
+                    return root;
+
+                // do the objectification
+                JArray arr = (JArray)root;
+                JObject res = new JObject();
+                foreach (JToken p in arr)
+                {
+                    JObject curr = (JObject)p;
+
+                    res.Add(curr["key"]!.ToString(), curr["value"]);
+
+                }
+                return res;
+            }
+            else
+            {
+                if (path[index].Equals(""))
+                    return Inner_Objectify(root, path, index + 1);
+
+                // path[index] != ""
+                if (root.Type == JTokenType.Object)
+                {
+                    JObject obj = (JObject)root;
+                    JToken r = obj[path[index]]!;
+
+                    if (r == null || r.Type == JTokenType.Null)
+                        return root;
+
+                    obj[path[index]] = Inner_Objectify(r, path, index + 1);
+                    return root;
+                }
+
+                if (root.Type == JTokenType.Array)
+                {
+                    // if we're at an array, simply apply to all elements
+                    JArray arr = (JArray)root;
+                    for (int i = 0; i < arr.Count; i++)
+                    {
+                        arr[i] = Inner_Objectify(arr[i], path, index);
+                    }
+                    return arr;
+                }
+
+                return root;
+            }
+        }
+        private JToken Inner_Listify(JToken root, string[] path, int index)
+        {
+
+            if (root.Type == JTokenType.Array)
+            {
+                // if we're at an array, simply apply to all elements
+                JArray arr = (JArray)root;
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    arr[i] = Inner_Listify(arr[i], path, index);
+                }
+                return arr;
+            }
+
+            if (path.Length == index)
+            {
+                // nothing to do if we're not at an object
+                if (root.Type != JTokenType.Object)
+                    return root;
+
+                // do the listification
+                JObject obj = (JObject)root;
+                JArray res = new JArray();
+                foreach (JProperty p in obj.Properties())
+                {
+                    JObject tmp = new JObject();
+                    tmp["key"] = p.Name;
+                    tmp["value"] = p.Value;
+                    res.Add(tmp);
+                }
+                return res;
+
+            }
+            else
+            {
+                if (path[index].Equals(""))
+                    return Inner_Listify(root, path, index + 1);
+
+                // path[index] != ""
+                if (root.Type == JTokenType.Object)
+                {
+                    JObject obj = (JObject)root;
+                    JToken r = obj[path[index]]!;
+
+                    if (r == null || r.Type == JTokenType.Null)
+                        return root;
+
+                    obj[path[index]] = Inner_Listify(r, path, index + 1);
+                    return root;
+                }
+
+                return root;
+            }
+        }
+    }
+}
---
+++ IArdoJSON.cs
@@ -0,0 +1,54 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Ardo.ArdoJSON
+{
+    /// <summary>
+    /// Version 1.0
+    /// Helper extension for JSON. Allows you to manipulate and parse JSON using JSONPath.
+    /// Uses the Newtonsoft.Json library (https://www.newtonsoft.com/json)
+    /// </summary>
+    [OSInterface(Description = "Version 0.1.0\r\n\r\nHelper extension for JSON. Allows you to manipulate and parse JSON using JSONPath.\r\n\r\nUses the Newtonsoft.Json library (https://www.newtonsoft.com/json)", IconResourceName = "ArdoJSON.resources.logo.png", Name = "ArdoJSON")]
+    public interface IArdoJSON
+    {
+        /// <summary>
+        /// Attempts to parse an IBAN, returning a boolean success indicator and a
+        /// new Iban structure. This method is exposed as a server action to your
+        /// ODC apps and libraries.
+        /// </summary>
+        [OSAction(Description = "Replaces an object identified by Path with a list of name/value pairs.\r\n\r\nThis is helpful to pre-process JSON with \"dynamic\" property names to something that can be translated into the OutSystems Platform types", IconResourceName = "ArdoJSON.resources.logo.png", ReturnName = "JSONOut")]
+        public string JSON_Listify(
+            [OSParameter(Description = "The JSON to be parsed")]
+            string JSONIn,
+            [OSParameter(Description = "Path to the point where processing should happen (where your object is)")]
+            string? Path="");
+
+        /// <summary>
+        /// Attempts to parse an IBAN, returning a boolean success indicator and a
+        /// new Iban structure. This method is exposed as a server action to your
+        /// ODC apps and libraries.
+        /// </summary>
+        [OSAction(Description = "Replaces an list of key/value pairs identified by Path with an object.\r\n\r\nThis is helpful to pre-process JSON with \"dynamic\" property names to something that can be translated into the OutSystems Platform types", IconResourceName = "ArdoJSON.resources.logo.png", ReturnName = "JSONOut")]
+        public string JSON_Objectify(
+            [OSParameter(Description = "The JSON to be parsed")]
+            string JSONIn,
+            [OSParameter(Description = "Path to the point where processing should happen (where your list of key/value pairs is)")]
+            string? Path= "");
+
+
+        /// <summary>
+        /// Attempts to parse an IBAN, returning a boolean success indicator and a
+        /// new Iban structure. This method is exposed as a server action to your
+        /// ODC apps and libraries.
+        /// </summary>
+        [OSAction(Description = "Allows simple path selection of an element in the JSON", IconResourceName = "ArdoJSON.resources.logo.png", ReturnName = "JSONOut")]
+        public string JSONSelect(
+            [OSParameter(Description = "The JSON to be parsed")]
+            string JSONIn,
+            [OSParameter(Description = "Path to select")]
+            string? Path=null,
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If true, when the JSON path returns a single element, force the content to return as JSON ( enclosed in double quotes). If false, return the value without the enclosing quotes. Defaults to false")]
+            bool? AlwaysReturnAsJSON =false);
+    }
+}
