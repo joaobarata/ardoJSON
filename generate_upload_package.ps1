a8897eaeb6575d16d3362aa62f1ee96b120a408e
João Barata <14177189+joaobarata@users.noreply.github.com>
2024-08-07 14:02:34 +0100

    Migrated to .Net 8.0

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,2 +1,2 @@
 dotnet publish -c Release -r linux-x64 --self-contained false
-Compress-Archive -Path .\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ArdoJSON.zip -force
+Compress-Archive -Path .\bin\Release\net8.0\linux-x64\publish\* -DestinationPath ArdoJSON.zip -force
